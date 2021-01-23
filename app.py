2ed425f434e1718ecc6da90065c80e16f90917c0
Gabriel Seet <malrhis@gmail.com>
2021-01-23 03:51:15 +0000

    complete testing of json extraction

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import json
 
 app = Flask(__name__)
+# database variable here is global
+database = {}
+with open('customers.json') as fp:
+    database = json.load(fp)
+print(database)
+
 
 @app.route('/')
 def home():
-    return "It's working!"
+    return render_template('home.template.html')
+
+
+@app.route('/customers')
+def show_customers():
+    return render_template('customers.template.html', customers=database)
+
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
